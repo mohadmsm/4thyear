2fa71a204f4880ff6b53c7383c3634f499d50162
Mohammed config test <mohadmnssnm@gmail.com>
2024-09-27 13:58:12 +0100

    added oop

--- 4th/OOP-EN1035/w1-3.cpp
+++ 4th/OOP-EN1035/w1-3.cpp
@@ -1,16 +1,36 @@
 #include<iostream>
 using namespace std;
+double sumdouble(double a, double b){
+	if (a != b)
+	return a+b;
+	else
+	return 2*(a+b);
+}
+string helloName(string name){
+	return "Hello "+name+"!";
+}
+bool monkyTrouble(bool a, bool b){
+	if (!a != !b) //xor operator
+	return true;
+	else
+	return false;
+}
 int main()
 {
-	int n;
-	cin>>n;
-	if(n%2==0)
-	{
-		cout<<"Even";
-	}
+/***	double a, b;
+	cout<<"Enter two numbers: ";
+	cin>>a>>b;
+	cout<<sumdouble(a,b)<<endl;
+	string name;
+	cout<<"Enter your name: ";
+	cin>>name;
+	cout<<helloName(name)<<endl;*/
+	bool c, d;
+	cout<<"Enter two booleans monkey: ";
+	cin>>c>>d;
+	if (monkyTrouble(c,d))
+	cout<<"We are in trouble"<<endl;
 	else
-	{
-		cout<<"Odd";
-	}
+	cout<<"We are not in trouble"<<endl;
 	return 0;
 }
