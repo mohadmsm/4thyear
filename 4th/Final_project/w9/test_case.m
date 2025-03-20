c652c9bac599d4ec8e38ba0f953217e2abd4761c
msi_mohad <mohadmnssnm@gmail.com>
2025-03-20 22:49:44 +0000

    added stuff

--- 4th/Final_project/w9/test_case.m
+++ 4th/Final_project/w9/test_case.m
@@ -11,7 +11,6 @@ f = 100e9;
 f = linspace(1,f,100);
 w = 2*pi*f;
 s = i *w; 
-R = R+Rs*(1+1j)*sqrt(w);
 % exact solution and generating 100 points 
 vo = sqrt(s*C.*(R+s*L))./(sqrt(s*C.*(R+s*L)).*cosh(l*sqrt(s*C.*(R+s*L)))+Rs*s*C.*sinh(l*sqrt(s*C.*(R+s*L))));
 % to be used in NILT
